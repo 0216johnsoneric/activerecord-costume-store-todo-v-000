8665108a4e85a9c218097e84e1f767c95eeb10d3
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-16 23:53:36 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :costume_inventory
+      t.timestamps null: false
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.
+
+    end
+  end
+end
