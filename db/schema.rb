0994f8f95c4f03e32e26d81db76b16b7666f838c
Eric Johnson <ericjohnson0216@icloud.com>
2019-10-17 00:23:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,6 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "costume_inventory"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "num_of_employees"
-  end
-
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
